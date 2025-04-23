4f6cd697d9abf4f7c82376744a6a44cc5786d742
Tyler <tydcarl@gmail.com>
2025-04-23 15:31:15 -0700

    distractions

---
+++ index.js
@@ -0,0 +1,15 @@
+//template_s3chdi9;
+//service_an0icci
+//f0SISkgznorqeV6fF
+
+function contact(event) {
+  event.preventDefault();
+  /*    emailjs.sendform(
+      "service_an0icci",
+      "template_s3chdi9, ",
+      event.target,
+      "f0SISkgznorqeV6fF"
+    ).then (() => { 
+        console.log('this worked1')
+    }) */
+}
